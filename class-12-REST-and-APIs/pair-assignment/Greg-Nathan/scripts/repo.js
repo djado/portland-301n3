b6e39fc0764c7775e4a2c124da442a9397b8b09d
gnkatchmar <gunk55@msn.com>
2016-10-01 13:43:03 -0700

    halfway point

--- class-12-REST-and-APIs/pair-assignment/Greg-Nathan/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/Greg-Nathan/scripts/repo.js
@@ -4,8 +4,21 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+    		url: 'https://api.github.com/user/repos',
+    		type: 'GET',
+    		headers: {
+    		'Authorization': 'token ' + githubToken
+    		},
+    		success: function(data){
+    			repos.all = data;
+    			callback();
+    		},
+    		error: function() {
+    			console.log('ERROR!');
+    		}
+    	});
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
